b3df3587d97ad707f973f974822c629252f9dbc2
3SPEDIATUZ <72047060+3SPEDIATUZ@users.noreply.github.com>
2022-09-27 11:29:25 -0500

    Creación del modelo.

---
+++ src/main/java/co/com/choucair/certification/proyectobase/model/Utest.java
@@ -0,0 +1,122 @@
+package co.com.choucair.certification.proyectobase.model;
+
+public class Utest {
+    /**
+     *declaration of variables
+     **/
+    private String strFirtsName;
+    private String strLastName;
+    private String strEmail;
+    private String strMoth;
+    private String strDay;
+    private String strYear;
+    private String strCity;
+    private String strZip;
+    private String strCountry;
+    private String strComputer;
+    private String strVersion;
+    private String strLanguage;
+    private String strMobileDevice;
+    private String strModel;
+    private String strOperatingSystem;
+    private String strPassword;
+    private String strRepassword;
+    private String question;
+    /**
+     *constructor creation
+     **/
+    public Utest(String strFirtsName, String strLastName, String strEmail, String strMoth, String strDay, String strYear, String strCity, String strZip, String strCountry, String strComputer, String strVersion, String strLanguage, String strMobileDevice, String strModel, String strOperatingSystem, String strPassword, String strRepassword, String question) {
+        this.strFirtsName = strFirtsName;
+        this.strLastName = strLastName;
+        this.strEmail = strEmail;
+        this.strMoth = strMoth;
+        this.strDay = strDay;
+        this.strYear = strYear;
+        this.strCity = strCity;
+        this.strZip = strZip;
+        this.strCountry = strCountry;
+        this.strComputer = strComputer;
+        this.strVersion = strVersion;
+        this.strLanguage = strLanguage;
+        this.strMobileDevice = strMobileDevice;
+        this.strModel = strModel;
+        this.strOperatingSystem = strOperatingSystem;
+        this.strPassword = strPassword;
+        this.strRepassword = strRepassword;
+        this.question = question;
+    }
+    /**
+     * Getter and Setter
+     **/
+    public String getStrFirtsName() {return strFirtsName;}
+
+    public void setStrFirtsName(String strFirtsName) {this.strFirtsName = strFirtsName;}
+
+    public String getStrLastName() {return strLastName;}
+
+    public void setStrLastName(String strLastName) {this.strLastName = strLastName;}
+
+    public String getStrEmail() {return strEmail;}
+
+    public void setStrEmail(String strEmail) {this.strEmail = strEmail;}
+
+    public String getStrMoth() {return strMoth;}
+
+    public void setStrMoth(String strMoth) {this.strMoth = strMoth;}
+
+    public String getStrDay() {return strDay;}
+
+    public void setStrDay(String strDay) {this.strDay = strDay;}
+
+    public String getStrYear() {return strYear;}
+
+    public void setStrYear(String strYear) {this.strYear = strYear;}
+
+    public String getStrCity() {return strCity;}
+
+    public void setStrCity(String strCity) {this.strCity = strCity;}
+
+    public String getStrZip() {return strZip;}
+
+    public void setStrZip(String strZip) {this.strZip = strZip;}
+
+    public String getStrCountry() {return strCountry;}
+
+    public void setStrCountry(String strCountry) {this.strCountry = strCountry;}
+
+    public String getStrComputer() {return strComputer;}
+
+    public void setStrComputer(String strComputer) {this.strComputer = strComputer;}
+
+    public String getStrVersion() {return strVersion;}
+
+    public void setStrVersion(String strVersion) {this.strVersion = strVersion;}
+
+    public String getStrLanguage() {return strLanguage;}
+
+    public void setStrLanguage(String strLanguage) {this.strLanguage = strLanguage;}
+
+    public String getStrMobileDevice() {return strMobileDevice;}
+
+    public void setStrMobileDevice(String strMobileDevice) {this.strMobileDevice = strMobileDevice;}
+
+    public String getStrModel() {return strModel;}
+
+    public void setStrModel(String strModel) {this.strModel = strModel;}
+
+    public String getStrOperatingSystem() {return strOperatingSystem;}
+
+    public void setStrOperatingSystem(String strOperatingSystem) {this.strOperatingSystem = strOperatingSystem;}
+
+    public String getStrPassword() {return strPassword;}
+
+    public void setStrPassword(String strPassword) {this.strPassword = strPassword;}
+
+    public String getStrRepassword() {return strRepassword;}
+
+    public void setStrRepassword(String strRepassword) {this.strRepassword = strRepassword;}
+
+    public String getQuestion() {return question;}
+
+    public void setQuestion(String question) {this.question = question;}
+}
